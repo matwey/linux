a2baa8f5398ecf37575eb5d925a0b72f206bc44c
Roger Quadros <rogerq@ti.com>
2013-06-18 19:04:47 +0300

    ARM: OMAP2+: Provide alias to USB PHY clock
    Till the OMAP clocks are correctly defined in device tree, use
    this temporary hack to provide clock alias to the USB PHY clocks.
    
    Without this, USB Host & Ethernet will not be functional with
    device tree boots on Panda and uEVM.
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    (cherry picked from commit 741532c4a995be11815cb72d4d7a48f442a22fea)

--- arch/arm/mach-omap2/board-generic.c
+++ arch/arm/mach-omap2/board-generic.c
@@ -15,6 +15,7 @@
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
 #include <linux/irqdomain.h>
+#include <linux/clk.h>
 
 #include <asm/mach/arch.h>
 
@@ -37,6 +38,20 @@ static struct of_device_id omap_dt_match_table[] __initdata = {
 
 // TEMP
 void __init omap_sgx_init_of(void);
+/*
+ * Create alias for USB host PHY clock.
+ * Remove this when clock phandle can be provided via DT
+ */
+static void __init legacy_init_ehci_clk(char *clkname)
+{
+	int ret;
+
+	ret = clk_add_alias("main_clk", NULL, clkname, NULL);
+	if (ret) {
+		pr_err("%s:Failed to add main_clk alias to %s :%d\n",
+						__func__, clkname, ret);
+	}
+}
 
 static void __init omap_generic_init(void)
 {
@@ -48,11 +63,17 @@ static void __init omap_generic_init(void)
 	 * HACK: call display setup code for selected boards to enable omapdss.
 	 * This will be removed when omapdss supports DT.
 	 */
-	if (of_machine_is_compatible("ti,omap4-panda"))
+	if (of_machine_is_compatible("ti,omap4-panda")) {
 		omap4_panda_display_init_of();
+		legacy_init_ehci_clk("auxclk3_ck");
+
+	}
 	else if (of_machine_is_compatible("ti,omap4-sdp"))
 		omap_4430sdp_display_init_of();
-    omap_sgx_init_of();
+	else if (of_machine_is_compatible("ti,omap5-uevm"))
+		legacy_init_ehci_clk("auxclk1_ck");
+
+	omap_sgx_init_of();
 }
 
 #ifdef CONFIG_SOC_OMAP2420
