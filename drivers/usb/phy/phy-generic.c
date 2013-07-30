a091df2b5d07da88dcdc5335869dac0ca1b1689f
Jingoo Han <jg1.han@samsung.com>
2013-07-30 17:02:13 +0900

    usb: phy: use dev_get_platdata()
    Use the wrapper function for retrieving the platform data instead of
    accessing dev->platform_data directly.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    (cherry picked from commit 19f9e188deb4bbcd5fc588f39dc63bdfa9103827)

--- drivers/usb/phy/phy-generic.c
+++ drivers/usb/phy/phy-generic.c
@@ -142,7 +142,8 @@ static int nop_set_host(struct usb_otg *otg, struct usb_bus *host)
 static int usb_phy_gen_xceiv_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct usb_phy_gen_xceiv_platform_data *pdata = pdev->dev.platform_data;
+	struct usb_phy_gen_xceiv_platform_data *pdata =
+			dev_get_platdata(&pdev->dev);
 	struct usb_phy_gen_xceiv	*nop;
 	enum usb_phy_type	type = USB_PHY_TYPE_USB2;
 	int err;
