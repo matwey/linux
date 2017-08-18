44ddb68163d6418b86c6d4836ec4f73e625ec130
NeilBrown <neilb@suse.com>
2017-08-18 13:12:12 +1000

    NFS: Don't flush caches for a getattr that races with writeback (bsc#1053933).
    # Conflicts:
    #	series.conf
    
    suse-commit: 78c8e32335136695217a86369fa69c1e80d923d0

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -1348,9 +1348,12 @@ static int nfs_update_inode(struct inode *inode, struct nfs_fattr *fattr)
 		if (nfsi->change_attr != fattr->change_attr) {
 			dprintk("NFS: change_attr change on server for file %s/%ld\n",
 					inode->i_sb->s_id, inode->i_ino);
-			invalid |= NFS_INO_INVALID_ATTR|NFS_INO_INVALID_DATA|NFS_INO_INVALID_ACCESS|NFS_INO_INVALID_ACL;
-			if (S_ISDIR(inode->i_mode))
-				nfs_force_lookup_revalidate(inode);
+			/* Could it be a race with writeback? */
+			if (nfsi->npages == 0) {
+				invalid |= NFS_INO_INVALID_ATTR|NFS_INO_INVALID_DATA|NFS_INO_INVALID_ACCESS|NFS_INO_INVALID_ACL;
+				if (S_ISDIR(inode->i_mode))
+					nfs_force_lookup_revalidate(inode);
+			}
 			nfsi->change_attr = fattr->change_attr;
 		}
 	} else if (server->caps & NFS_CAP_CHANGE_ATTR)
