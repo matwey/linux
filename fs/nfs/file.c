d494f66a66a1df1726f970feb27a4321eab6847f
NeilBrown <neilb@suse.com>
2017-08-18 13:12:12 +1000

    NFS: invalidate file size when taking a lock (bsc#1053933).
    suse-commit: a2f521d37005121e65e63409bb4fcc1d6a2353ce

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -861,7 +861,7 @@ do_setlk(struct file *filp, int cmd, struct file_lock *fl, int is_local)
 	 */
 	nfs_sync_mapping(filp->f_mapping);
 	if (!nfs_have_delegation(inode, FMODE_READ))
-		nfs_zap_mapping(inode, filp->f_mapping);
+		nfs_zap_caches(inode);
 out:
 	return status;
 }
