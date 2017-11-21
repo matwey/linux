0d91532b45e74fc9bfddb1783f099c4e476e2dd8
Takashi Iwai <tiwai@suse.de>
2017-11-21 07:55:55 +0100

    kABI fix for 4.4.99 net changes (stable-4.4.99).
    suse-commit: d0026d025a1d8a5271189d4359e8d607d70b0bb8

--- include/net/inet_sock.h
+++ include/net/inet_sock.h
@@ -95,7 +95,11 @@ struct inet_request_sock {
 	kmemcheck_bitfield_end(flags);
 	u32                     ir_mark;
 	union {
+#ifdef __GENKSYMS__
+		struct ip_options_rcu	*opt;
+#else
 		struct ip_options_rcu __rcu	*ireq_opt;
+#endif
 		struct sk_buff		*pktopts;
 	};
 };
