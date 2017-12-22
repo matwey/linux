bc61b86a5487f83da19ab62e561870b86d6385b3
Michal Suchanek <msuchanek@suse.de>
2017-12-22 01:02:42 +0100

    Update patches.suse/powerpc-rfi-flush.patch (bsc#1068032).
    suse-commit: 02d77cb11e65f25940d30ba16a18de39e1b60577

--- arch/powerpc/include/asm/setup.h
+++ arch/powerpc/include/asm/setup.h
@@ -3,4 +3,11 @@
 
 #include <asm-generic/setup.h>
 
+#ifndef __ASSEMBLY__
+
+void rfi_flush_enable(bool enable);
+void __init setup_rfi_flush(void);
+
+#endif /* !__ASSEMBLY__ */
+
 #endif	/* _ASM_POWERPC_SETUP_H */
