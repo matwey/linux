45e8985d7962b77b28e5e61d95a9903b2def4ac8
Michal Suchanek <msuchanek@suse.de>
2018-01-29 18:54:36 +0100

    powerpc/pseries: rfi-flush: Call setup_rfi_flush() after LPM migration (bsc#1068032, bsc#1075088).
    - Delete patches.fixes/0004-powerpc-rfi-flush-Make-setup_rfi_flush-not-__init.patch.
    - Delete patches.fixes/0006-powerpc-pseries-rfi-flush-Call-setup_rfi_flush-after.patch.
    
    suse-commit: 72e5c9c017d413e25f0314d75b9c31a972314087

--- arch/powerpc/kernel/setup_64.c
+++ arch/powerpc/kernel/setup_64.c
@@ -720,10 +720,23 @@ void rfi_flush_enable(bool enable)
 	rfi_flush = enable;
 }
 
-static void init_fallback_flush(void)
+static bool init_fallback_flush(void)
 {
 	u64 l1d_size, limit;
 	int cpu;
+
+	if (l1d_flush_fallback_area)
+		return true;
+
+	/*
+	 * Once the slab allocator is up it's too late to allocate the fallback
+	 * flush area, so return an error. This should not really happen
+	 * because we call the init_fallback_flush() early. Doing the
+	 * allocation later might fail due to memory fragmentation so we want
+	 * to avoid that.
+	 */
+	if (slab_is_available())
+		return false;
 
 	l1d_size = ppc64_caches.dsize;
 	limit = min(safe_stack_limit(), ppc64_rma_size);
@@ -752,13 +765,26 @@ static void init_fallback_flush(void)
 		paca_aux->l1d_flush_congruence = c;
 		paca_aux->l1d_flush_sets = c / 128;
 	}
+
+	return true;
 }
 
 void setup_rfi_flush(enum l1d_flush_type types, bool enable)
 {
-	if (types & L1D_FLUSH_FALLBACK) {
-		pr_info("rfi-flush: Using fallback displacement flush\n");
+	/*
+	 * Allocate the fallback area early during boot even if the detected
+	 * flush type doe not need it - it might change due to migration.
+	 */
+	if (!no_rfi_flush)
 		init_fallback_flush();
+
+	if (types & L1D_FLUSH_FALLBACK) {
+		if (init_fallback_flush())
+			pr_info("rfi-flush: Using fallback displacement flush\n");
+		else {
+			pr_crit("rfi-flush: Error unable to use fallback displacement flush!\n");
+			types &= ~L1D_FLUSH_FALLBACK;
+		}
 	}
 
 	if (types & L1D_FLUSH_ORI)
