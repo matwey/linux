75098886b1ce471a78265841af92e3fe1a418e82
Borislav Petkov <bp@suse.de>
2018-01-31 21:49:52 +0100

    x86/spectre_v2: nospectre_v2 means nospec too (bsc#1075994 bsc#1075091).
    suse-commit: ae2b9c9e466ab7950390fed8c349de4cec4d6c69

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -12,6 +12,7 @@
 #include <linux/device.h>
 
 #include <asm/nospec-branch.h>
+#include <asm/spec_ctrl.h>
 #include <asm/cmdline.h>
 #include <asm/bugs.h>
 #include <asm/processor.h>
@@ -315,6 +316,7 @@ static enum spectre_v2_mitigation_cmd __init spectre_v2_parse_cmdline(void)
 	if (!cmdline_find_option_bool(boot_command_line, "nospectre_v2"))
 		return SPECTRE_V2_CMD_AUTO;
 disable:
+	nospec("");
 	spec2_print_if_insecure("disabled on command line.");
 	return SPECTRE_V2_CMD_NONE;
 }
