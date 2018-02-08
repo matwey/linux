7ccbcb7eb30557c17a3dc7f6a2e912a271ece9d0
Oliver Neukum <oneukum@suse.com>
2018-02-08 14:10:24 +0100

    - usbip: fix stub_rx: get_pipe() to validate endpoint number   (bsc#1078673 CVE-2017-16912). - Refresh   patches.fixes/0001-usbip-fix-stub_rx-harden-CMD_SUBMIT-path-to-handle-m.patch.
    suse-commit: 264631b7ca7182c7cc8e2eddfffd1f3ff369f567

--- drivers/staging/usbip/stub_rx.c
+++ drivers/staging/usbip/stub_rx.c
@@ -359,15 +359,15 @@ static int get_pipe(struct stub_device *sdev, struct usbip_header *pdu)
 	int epnum = pdu->base.ep;
 	int dir = pdu->base.direction;
 
+	if (epnum < 0 || epnum > 15)
+		goto err_ret;
+
 	if (dir == USBIP_DIR_IN)
 		ep = udev->ep_in[epnum & 0x7f];
 	else
 		ep = udev->ep_out[epnum & 0x7f];
-	if (!ep) {
-		dev_err(&sdev->interface->dev, "no such endpoint?, %d\n",
-			epnum);
-		BUG();
-	}
+	if (!ep)
+		goto err_ret;
 
 	epd = &ep->desc;
 #if 0
@@ -431,9 +431,10 @@ static int get_pipe(struct stub_device *sdev, struct usbip_header *pdu)
 			return usb_rcvisocpipe(udev, epnum);
 	}
 
+err_ret:
 	/* NOT REACHED */
-	dev_err(&sdev->interface->dev, "CMD_SUBMIT: invalid epnum %d\n", epnum);
-	return 0;
+	dev_err(&sdev->interface->dev, "invalid epnum %d\n", epnum);
+	return -1;
 }
 
 static void masking_bogus_flags(struct urb *urb)
@@ -498,6 +499,9 @@ static void stub_recv_cmd_submit(struct stub_device *sdev,
 	struct usbip_device *ud = &sdev->ud;
 	struct usb_device *udev = sdev->udev;
 	int pipe = get_pipe(sdev, pdu);
+
+	if (pipe == -1)
+		return;
 
 	priv = stub_priv_alloc(sdev, pdu);
 	if (!priv)
