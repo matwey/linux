9a9ae9d33dd40ef262bc6542f7574baeee3461fb
Hannes Reinecke <hare@suse.de>
2018-03-06 13:21:55 +0100

    nvme-rdma: fix concurrent reset and reconnect (bsc#1082979).
    suse-commit: b6b1c49ed0904ccc6e81083183a74cab068ccde1

--- drivers/nvme/host/rdma.c
+++ drivers/nvme/host/rdma.c
@@ -835,12 +835,18 @@ static void nvme_rdma_error_recovery_work(struct work_struct *work)
 	blk_mq_kick_requeue_list(ctrl->ctrl.admin_q);
 	nvme_start_queues(&ctrl->ctrl);
 
+	if (!nvme_change_ctrl_state(&ctrl->ctrl, NVME_CTRL_RECONNECTING)) {
+		/* state change failure should never happen */
+		WARN_ON_ONCE(1);
+		return;
+	}
+
 	nvme_rdma_reconnect_or_remove(ctrl);
 }
 
 static void nvme_rdma_error_recovery(struct nvme_rdma_ctrl *ctrl)
 {
-	if (!nvme_change_ctrl_state(&ctrl->ctrl, NVME_CTRL_RECONNECTING))
+	if (!nvme_change_ctrl_state(&ctrl->ctrl, NVME_CTRL_RESETTING))
 		return;
 
 	queue_work(nvme_rdma_wq, &ctrl->err_work);
@@ -1729,6 +1735,12 @@ static void nvme_rdma_reset_ctrl_work(struct work_struct *work)
 	bool changed;
 
 	nvme_rdma_shutdown_ctrl(ctrl);
+
+	if (!nvme_change_ctrl_state(&ctrl->ctrl, NVME_CTRL_RECONNECTING)) {
+		/* state change failure should never happen */
+		WARN_ON_ONCE(1);
+		return;
+	}
 
 	ret = nvme_rdma_configure_admin_queue(ctrl);
 	if (ret) {
