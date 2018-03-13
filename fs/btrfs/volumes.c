36457f32cc264c8def0fb81f3829beeb0eb76933
Jeff Mahoney <jeffm@suse.com>
2018-03-12 22:27:52 -0400

    Btrfs: stop refusing the relocation of chunk 0 (bsc#1089208).
    suse-commit: d3271c7471a1f7746200a3495a48b1ef46ff287e

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -3001,10 +3001,6 @@ again:
 		if (found_key.objectid != key.objectid)
 			break;
 
-		/* chunk zero is special */
-		if (found_key.offset == 0)
-			break;
-
 		chunk = btrfs_item_ptr(leaf, slot, struct btrfs_chunk);
 
 		if (!counting) {
@@ -3040,6 +3036,8 @@ again:
 			spin_unlock(&fs_info->balance_lock);
 		}
 loop:
+		if (found_key.offset == 0)
+			break;
 		key.offset = found_key.offset - 1;
 	}
 
