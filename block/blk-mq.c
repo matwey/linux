75f4936bd45464b0cb160b4e1d4cefb70a0d21a4
Hannes Reinecke <hare@suse.de>
2018-03-14 14:16:57 +0100

    blk-mq: stop 'delayed_run_work' in blk_mq_stop_hw_queue() (bsc#1084967).
    suse-commit: dad553a2cdc7697440418260b5ceaea369bf63d6

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1091,6 +1091,7 @@ void blk_mq_stop_hw_queue(struct blk_mq_hw_ctx *hctx)
 {
 	cancel_work(&hctx->run_work);
 	cancel_delayed_work(&hctx->delay_work);
+	cancel_delayed_work(&hctx->delayed_run_work);
 	set_bit(BLK_MQ_S_STOPPED, &hctx->state);
 }
 EXPORT_SYMBOL(blk_mq_stop_hw_queue);
