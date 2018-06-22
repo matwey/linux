b64ca0c012d260314d7a925ebed45738b8fcd25a
Borislav Petkov <bp@suse.de>
2018-06-22 19:54:54 +0200

    x86/cpu/topology: Provide detect_extended_topology_early() (bsc#1089343).
    suse-commit: b953ead18a5bb576cdb7d352701ecbf20dcda5b1

--- arch/x86/kernel/cpu/cpu.h
+++ arch/x86/kernel/cpu/cpu.h
@@ -47,6 +47,7 @@ extern void get_cpu_cap(struct cpuinfo_x86 *c);
 extern void cpu_detect_cache_sizes(struct cpuinfo_x86 *c);
 extern void x86_spec_ctrl_setup_ap(void);
 
+extern int detect_extended_topology_early(struct cpuinfo_x86 *c);
 extern int detect_ht_early(struct cpuinfo_x86 *c);
 
 #endif /* ARCH_X86_CPU_H */
