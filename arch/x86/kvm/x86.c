75535fd4da2e933afb0f0b13e7e68b8b388fe5e1
Jiri Kosina <jkosina@suse.cz>
2018-07-06 17:05:43 +0200

    kvm: x86: mark kvm_write_guest_virt_system() as unconfined (bnc#1089343, CVE-2018-3646).
    suse-commit: 9c2cd4dbd4f1c173392730e3b7da6cde3bcff086

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4393,6 +4393,9 @@ static int emulator_write_std(struct x86_emulate_ctxt *ctxt, gva_t addr, void *v
 int kvm_write_guest_virt_system(struct kvm_vcpu *vcpu, gva_t addr, void *val,
 				unsigned int bytes, struct x86_exception *exception)
 {
+	/* kvm_write_guest_virt_system can pull in tons of pages. */
+	vcpu->arch.vcpu_unconfined = true;
+
 	return kvm_write_guest_virt_helper(addr, val, bytes, vcpu,
 					   PFERR_WRITE_MASK, exception);
 }
