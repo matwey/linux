6e8bcd20824da353db0a54eeacb12a88a42ef9f0
Michal Hocko <mhocko@suse.com>
2018-07-24 14:33:32 +0200

    - Refresh patches.arch/x86-l1tf-04-protect-PROT_NONE-ptes.patch. - Refresh   patches.arch/x86-speculation-l1tf-fix-up-pte-pfn-conversion-for-P.patch.
    make sure to restrict pmd clobbering to outside of reserved bits and use
    PHYSICAL_PMD_PAGE_MASK where appropriate
    
    suse-commit: 2691d43c7229df14c4ddef406342b989606bcd81

--- arch/x86/include/asm/page_types.h
+++ arch/x86/include/asm/page_types.h
@@ -16,6 +16,7 @@
    virtual addresses are 32-bits but physical addresses are larger
    (ie, 32-bit PAE). */
 #define PHYSICAL_PAGE_MASK	(((signed long)PAGE_MASK) & __PHYSICAL_MASK)
+#define PHYSICAL_PMD_PAGE_MASK	(((signed long)PMD_PAGE_MASK) & __PHYSICAL_MASK)
 
 #define PMD_PAGE_SIZE		(_AC(1, UL) << PMD_SHIFT)
 #define PMD_PAGE_MASK		(~(PMD_PAGE_SIZE-1))
