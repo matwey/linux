9d4ce13f391d16229152dd9f0b589eafdd19fdf6
Matwey V. Kornilov <matwey@sai.msu.ru>
2021-01-30 12:53:00 +0300

    hwmon: lm75: Handle broken device nodes gracefully
    There is a logical flaw in lm75_probe() function introduced in
    
        e97a45f1b460 ("hwmon: (lm75) Add OF device ID table")
    
    Note, that of_device_get_match_data() returns NULL when no match
    found. This is the case when OF node exists but has unknown
    compatible line, while the module is still loaded via i2c
    detection.
    
    arch/powerpc/boot/dts/fsl/p2041rdb.dts:
    
        lm75b@48 {
        	compatible = "nxp,lm75a";
        	reg = <0x48>;
        };
    
    In this case, the sensor is mistakenly considered as ADT75 variant.
    
    Fixes: e97a45f1b460 ("hwmon: (lm75) Add OF device ID table")
    Signed-off-by: Matwey V. Kornilov <matwey@sai.msu.ru>

--- drivers/hwmon/lm75.c
+++ drivers/hwmon/lm75.c
@@ -561,9 +561,15 @@ static int lm75_probe(struct i2c_client *client)
 	int status, err;
 	enum lm75_type kind;
 
-	if (client->dev.of_node)
-		kind = (enum lm75_type)of_device_get_match_data(&client->dev);
-	else
+	if (dev->of_node) {
+		const struct of_device_id* match =
+			of_match_device(dev->driver->of_match_table, dev);
+
+		if (!match)
+			return -ENODEV;
+
+		kind = (enum lm75_type)(match->data);
+	} else
 		kind = i2c_match_id(lm75_ids, client)->driver_data;
 
 	if (!i2c_check_functionality(client->adapter,
